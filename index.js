f148a63ef1ce2e7b7b2e69d8083d63fe0a5e9724
Yulia Tertyshnyk <88549376+fraujulia@users.noreply.github.com>
2021-11-30 10:39:24 +0200

    corrected output result

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 let firstNumber = prompt ('First Number');
 let secondNumber = prompt ('Second Number');
 let result = +firstNumber + +secondNumber;
-alert (`${result}`);
+alert (`${firstNumber} + ${secondNumber} = ${result}`);
